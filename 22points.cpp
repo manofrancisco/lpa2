9de87cdb4878db006a4dd7639d68feb46f78ecd2
Francisco Martins Ferreira <fmferreira@student.dei.uc.pt>
2018-03-13 17:15:52 +0000

    done

--- 22points.cpp
+++ 22points.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <vector>
- class Group{
- public:
-     Group() {}
-     int state;
-     int size;
- };
+class Group{
+public:
+    Group() {}
+    int state;
+    int size;
+};
 
 std::vector<Group> groups;
 
-std::vector<std::vector<std::vector<int> > > matrix;
+int max_changes;
+int num_groups;
+
+int start;
+int flower_count;
+
+int best;
 
 
-
-std::vector<std::vector<std::vector<int> > > initialize_matrix(int num_groups,int max_changes){
+std::vector<std::vector<std::vector<int> > > initialize_matrix(){
     std::vector <int> change_vec((unsigned long)max_changes+1);
-    for(int i = 0; i < max_changes;i++){
-        change_vec[i] = 1501;
-    }
     std::vector < std::vector <int> > group_vec((unsigned long)num_groups,change_vec);
     std::vector<std::vector<std::vector<int> > >m((unsigned long) 2,group_vec);
 
@@ -33,59 +35,58 @@ std::vector<std::vector<std::vector<int> > > initialize_matrix(int num_groups,in
     return m;
 }
 
-int fertilize(int state,int n_group,int n_changes,int num_groups,int max_changes,std::vector<std::vector<std::vector<int> > >* m){
-    if((*m)[state][n_group][n_changes] != 1501){
-        return (*m)[state][n_group][n_changes];
+int fertilize(int state,int n_group,int n_changes,std::vector<std::vector<std::vector<int> > > m){
+    if(m[state][n_group][n_changes] != 1501){
+        return m[state][n_group][n_changes];
     }
     Group current = groups[n_group];
     if(n_group == num_groups -1){
 
         if(current.state == state){
-            (*m)[state][n_group][n_changes] = 0;
+            m[state][n_group][n_changes] = 0;
         }else{
-            (*m)[state][n_group][n_changes] = current.size;
+            m[state][n_group][n_changes] = current.size;
         }
-        return (*m)[state][n_group][n_changes];
+        return m[state][n_group][n_changes];
     }else{
 
         int wrong = ((state == current.state)? 0:current.size);
-        int no_change = fertilize(state,n_group+1,n_changes,num_groups,max_changes,m);
+        int no_change = fertilize(state,n_group+1,n_changes,m);
         int change = 1501;
-        if(n_changes < max_changes && current.state != groups[n_group+1].state){
+        if(n_changes < max_changes && state != current.state){
             int new_state = ((current.state == 0)? 1:0);
-            change = fertilize(new_state,n_group+1,n_changes+1,num_groups,max_changes,m);
-        }else{
+            change = fertilize(new_state,n_group+1,n_changes+1,m);
         }
-        (*m)[state][n_group][n_changes] = wrong + ((change > no_change)? no_change:change);
-
-        return (*m)[state][n_group][n_changes];
+        m[state][n_group][n_changes] = wrong + ((change > no_change)? no_change:change);
+        return m[state][n_group][n_changes];
     }
 }
 
 
-int www(int num_groups,int max_changes){
+int www(){
+    best = 1501;
     if(max_changes >= num_groups){
         return 0;
     }
-    matrix = initialize_matrix(num_groups,max_changes);
+    std::vector<std::vector<std::vector<int> > > m;
+    m = initialize_matrix();
     int start_1, start_2,new_state;
     Group first = groups[0];
-    start_1 = fertilize(first.state,0,1,num_groups,max_changes,&matrix);
+    start_1 = fertilize(first.state,1,1,m);
     new_state = ((first.state == 1)? 0:1);
-
-    start_2 = fertilize(new_state,0,1,num_groups,max_changes,&matrix);
+    start_2 = first.size + fertilize(new_state,1,1,m);
     return ((start_1 < start_2)? start_1: start_2);
 }
 
 
 void input(){
     std::string str;
-    int  max_changes,num_groups;
-    int best;
+    max_changes = 0;
     while(std::getline (std::cin,str)){
         groups.clear();
         max_changes = 0;
         num_groups = 0;
+        flower_count = 0;
         int i;
         for(i = 0; i<(int)str.size();i++){
             if(str[i]!=' '){
@@ -94,6 +95,7 @@ void input(){
         }
         int counter = 1;
         int state = str[i+1]-48;
+        start = state;
         for(i= i+2;i<(int)str.size();i++){
             if(((int)str[i]-48) != state){
                 Group r;
@@ -106,13 +108,14 @@ void input(){
             }else{
                 counter++;
             }
+            flower_count++;
         }
         Group r;
         r.size = counter;
         r.state = state;
         groups.push_back(r);
         num_groups++;
-        best = www(num_groups,max_changes);
+        int best = www();
         std::cout << best << '\n';
     }
 }
@@ -120,6 +123,8 @@ void input(){
 
 int main() {
 
+    num_groups = 0;
+    flower_count=0;
     input();
 
     return 0;
--- main.cpp
+++ main.cpp
@@ -9,16 +9,11 @@
 
 std::vector<Group> groups;
 
-int max_changes;
-int num_groups;
-
-int start;
-int flower_count;
-
-int best;
+std::vector<std::vector<std::vector<int> > > matrix;
 
 
-std::vector<std::vector<std::vector<int> > > initialize_matrix(){
+
+std::vector<std::vector<std::vector<int> > > initialize_matrix(int num_groups,int max_changes){
     std::vector <int> change_vec((unsigned long)max_changes+1);
     for(int i = 0; i < max_changes;i++){
         change_vec[i] = 1501;
@@ -41,57 +36,56 @@ std::vector<std::vector<std::vector<int> > > initialize_matrix(){
     return m;
 }
 
-int fertilize(int state,int n_group,int n_changes,std::vector<std::vector<std::vector<int> > > m){
-    Group current = groups[n_group];
-    if(m[state][n_group][n_changes] != 1501){
-        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State already in the table: "<< m[state][n_group][n_changes] << '\n';
-        return m[state][n_group][n_changes];
+int fertilize(int state,int n_group,int n_changes,int num_groups,int max_changes,std::vector<std::vector<std::vector<int> > >* m){
+    if((*m)[state][n_group][n_changes] != 1501){
+        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State already in the table: "<< (*m)[state][n_group][n_changes] << '\n';
+        return (*m)[state][n_group][n_changes];
     }
+    Group current = groups[n_group];
     if(n_group == num_groups -1){
+
         std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "Last group achieved!" << '\n';
         if(current.state == state){
-            m[state][n_group][n_changes] = 0;
+            (*m)[state][n_group][n_changes] = 0;
         }else{
-            m[state][n_group][n_changes] = current.size;
+            (*m)[state][n_group][n_changes] = current.size;
         }
-        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "->" << m[state][n_group][n_changes] << '\n';
-        return m[state][n_group][n_changes];
+        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "->" << (*m)[state][n_group][n_changes] << '\n';
+        return (*m)[state][n_group][n_changes];
     }else{
         std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State not in table!" << '\n';
 
         int wrong = ((state == current.state)? 0:current.size);
-        int no_change = fertilize(state,n_group+1,n_changes,m);
+        int no_change = fertilize(state,n_group+1,n_changes,num_groups,max_changes,m);
         int change = 1501;
-        if(n_changes < max_changes){
+        if(n_changes < max_changes && current.state != groups[n_group+1].state){
             int new_state = ((current.state == 0)? 1:0);
-            change = fertilize(new_state,n_group+1,n_changes+1,m);
+            change = fertilize(new_state,n_group+1,n_changes+1,num_groups,max_changes,m);
         }else{
             std::cout << "No more changes allowed!\n";
         }
-        m[state][n_group][n_changes] = wrong + ((change > no_change)? no_change:change);
-        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State calculated: "<< m[state][n_group][n_changes] << '\n';
+        (*m)[state][n_group][n_changes] = wrong + ((change > no_change)? no_change:change);
+        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State calculated: "<<(*m)[state][n_group][n_changes] << '\n';
 
-        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State already in the table: "<< m[state][n_group][n_changes] << '\n';
-        return m[state][n_group][n_changes];
+        std::cout << '['<< state << ','<<n_group << ',' << n_changes<<']' << "State already in the table: "<< (*m)[state][n_group][n_changes] << '\n';
+        return (*m)[state][n_group][n_changes];
     }
 }
 
 
-int www(){
-    best = 1501;
+int www(int num_groups,int max_changes){
     if(max_changes >= num_groups){
         return 0;
     }
     std::cout << "\n\n" <<"Numero de grupos: " << num_groups <<"\n\n";
-    std::vector<std::vector<std::vector<int> > > m;
-    m = initialize_matrix();
+    matrix = initialize_matrix(num_groups,max_changes);
     int start_1, start_2,new_state;
     Group first = groups[0];
-    start_1 = fertilize(first.state,1,1,m);
+    start_1 = fertilize(first.state,0,1,num_groups,max_changes,&matrix);
     std::cout << '['<< first.state << ','<< 0 << ',' << 1<<']' << "First with right state: "<< start_1 << '\n';
     new_state = ((first.state == 1)? 0:1);
 
-    start_2 = first.size + fertilize(new_state,1,1,m);
+    start_2 = fertilize(new_state,0,1,num_groups,max_changes,&matrix);
     std::cout << '['<< new_state << ','<< 0 << ',' << 1<<']' << "First with wrong state: "<< start_2 << " First size: "<< first.size<<'\n';
     return ((start_1 < start_2)? start_1: start_2);
 }
@@ -99,15 +93,15 @@ int www(){
 
 void input(){
     std::string str;
-    max_changes = 0;
+    int  max_changes,num_groups;
+    int best;
     while(std::getline (std::cin,str)){
         groups.clear();
         max_changes = 0;
         num_groups = 0;
-        flower_count = 0;
         std::cout << str <<"\n";
         int i;
-        for(i = 0; i<str.size();i++){
+        for(i = 0; i<(int)str.size();i++){
             if(str[i]!=' '){
                 max_changes = 10*max_changes + ((int)str[i]-48);
             }else break;
@@ -115,30 +109,28 @@ void input(){
         std::cout << "Numero de trocas permitidas: " << max_changes << "\n";
         int counter = 1;
         int state = str[i+1]-48;
-        start = state;
-        for(i= i+2;i<str.size();i++){
+        for(i= i+2;i<(int)str.size();i++){
             if(((int)str[i]-48) != state){
                 Group r;
                 r.size = counter;
                 r.state = state;
                 groups.push_back(r);
-                std::cout << "Linha de tulipas num:" << state << " com "<< counter << "flores  \n";
+                std::cout << "Linha de tulipas num: " << state << " com "<< counter << "flores  \n";
                 num_groups++;
                 state = (int)str[i]-48;
                 counter = 1;
             }else{
                 counter++;
             }
-            flower_count++;
         }
         Group r;
         r.size = counter;
         r.state = state;
         groups.push_back(r);
         num_groups++;
-        std::cout << "Linha de tulipas num:" << state << " com "<< counter << "flores  \n";
-        std::cout << " Numero total de linhas: " << num_groups <<"\n Numero total de flores: "<<flower_count<<"\n";
-        int best = www();
+        std::cout << "Linha de tulipas num: " << state << " com "<< counter << "flores  \n";
+        std::cout << " Numero total de grupos: " << num_groups <<"\n";
+        best = www(num_groups,max_changes);
         std::cout << "Melhor possivel: " << best << '\n';
     }
 }
@@ -147,8 +139,6 @@ void input(){
 int main() {
 
     std::cout << "Hello, World!" << std::endl;
-    num_groups = 0;
-    flower_count=0;
     input();
 
     return 0;
